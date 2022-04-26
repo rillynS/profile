44d0d0d53a125f24891b4ebe9eff57fdfafbeb67
freshrg-admin <korshunov.dv@freshrg.ru>
2022-04-26 14:03:49 +0300

    add reverse animation

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -10,13 +10,6 @@ export const Cards:FC = () => {
             <CardItem2/>
             <CardItem2/>
             <CardItem2/>
-            {/* <CardItem2/>
-            <CardItem2/>
-            <CardItem2/>
-            <CardItem2/>
-            <CardItem2/>
-            <CardItem2/> */}
-        {/* <CardItem description="Приложение по формату списка контактов с возможностью добавления, удаления, изменения контакта" title="Contact List" imgSrc="http://localhost:3000/static/media/background.032cb00af1c406037fd5.jpg"/> */}
         </div>
         )
 }
